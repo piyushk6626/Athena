45e86249c6da44c4a962ff40f95312e8c783ea79
piyushk6626 <kulkarnipiyush462@gmail.com>
2025-02-07 19:24:15 +0530

    returent result updated

--- resturent/search.py
+++ resturent/search.py
@@ -112,8 +112,11 @@ def find_similar_items(query: str, number_of_results: int = 5) -> list:
     # Get the Pinecone index using a helper function
     index = get_index()
 
+    #update the query based on given format 
+    explained_query=explain_UserQuery(query)
+
     # Generate vector embeddings for the query string
-    vector = create_embeddings(query)
+    vector = create_embeddings(explained_query)
 
     # Query the Pinecone index with the generated embeddings
     results = query_index(index, vector, number_of_results)
@@ -121,8 +124,46 @@ def find_similar_items(query: str, number_of_results: int = 5) -> list:
     # Return the list of results
     return results
 
+SystemPrompt="""Write a concise description to help the user find a restaurant based on their query and the following points:
+
+1. **Ambiance & Design**:
+   - Décor, lighting, and standout features from photos.
+   - Music or atmosphere insights from reviews.
+
+2. **Menu & Culinary Experience**:
+   - Signature dishes and specialties.
+   - Food presentation and dietary options if available.
+
+3. **Customer Experience & Service**:
+   - Staff friendliness and service efficiency.
+   - Information on reservation or walk-in policies.
+
+4. **Pricing**:
+   - Consider user preferences for cost. 
+"""
+
+def explain_UserQuery(query: str) -> str :
+    messages=[
+            {"role": "system", "content": SystemPrompt},
+            {"role": "user", "content": query}
+        ]
+    
+    Explained_Qury=client.chat.completions.create(
+        messages=messages,
+        model="gpt-4o-mini",
+        temperature=0.9
+        
+    )
+        
+        
+    
+    return Explained_Qury.choices[0].message.content
+    
+    
+    
+    
 
 if __name__ == "__main__":
-    Output=find_similar_items("kokni food with grate ambiance")
+    Output=find_similar_items("Protugese food chep price")
 
     print(Output)
